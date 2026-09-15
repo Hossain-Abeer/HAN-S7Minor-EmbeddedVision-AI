6445e83015e732ca6d28e6760a83ba9f24699e58
Hossain Abeer <akm.h.abeer@gmail.com>
2026-09-15 13:59:38 +0200

    main.cpp codblocks explanation

--- IPO/HelloVision/main.cpp
+++ IPO/HelloVision/main.cpp
@@ -26,15 +26,28 @@ int main()
 
        // Create bit image
        Mat dst(width,height,CV_8UC1);
+       //new mat container named 'dst' created, memory allocated
+       //assigned dimesnsions: widht, height CV_8UC1
+       //'width' and 'height' were previously defined
+       //CV_8UC1 → a label representing greyscale (8-bit, 1 channel) 
 
        // Threshold
        threshold(src,dst,120,255,THRESH_BINARY);
+       /*
+       src → input grayscale.
+       dst → output Mat (same size/type).
+       thresh = 120 → cutoff value.
+       maxval = 255 → value assigned when condition true.
+       threshold() → function.
+       THRESH_BINARY → enum.
+       Function scans every pixel, applies rule, writes to dst.
+       */
 
        // Create a window
        namedWindow("Threshold", WINDOW_AUTOSIZE);
        moveWindow("Threshold", 450, 100);
 
-       // Show the image
+       // Show the image into window
        imshow("Threshold",dst);
       
        while (waitKey(10)!=27); 
